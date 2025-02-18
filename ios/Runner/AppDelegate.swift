bb704e892761e6e6be9a7138f0e8d4fa52bdae2d
disdesoft <disdesoft@gmail.com>
2025-02-17 22:48:02 -0500

    reinstalar carpeta ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,14 +1,13 @@
-import UIKit
 import Flutter
-import Firebase
+import UIKit
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    FirebaseApp.configure()  // 🔥 Agregar esta línea
+    GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
